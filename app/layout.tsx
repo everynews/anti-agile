f9a16b3b1e007a81539b9e3a4dbbca95d44b174e
Sunghyun Cho <hey@cho.sh>
2025-04-19 15:02:14 +0900

    feat: Add Open Graph images and videos to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,23 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: "Dear Vercel, I'll quit being Anti-Agile",
   description: "An interactive essay on why I'll quit being Anti-Agile",
-  generator: 'v0.dev',
+  openGraph: {
+    images: [
+      {
+        url: '/antiagile.png',
+        width: 3456,
+        height: 1984,
+        alt: 'Dear Vercel, I\'ll quit being Anti-Agile',
+      },
+    ],
+    videos: [
+      {
+        url: '/antiagile.mp4',
+        width: 1920,
+        height: 1080,  
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
